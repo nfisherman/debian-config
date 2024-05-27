afc53521d3579bda151f30793c715b9e51e0d561
nfisherman <ca.lehman05@gmail.com>
2024-05-27 11:19:25 +0000

    removed pipx dependency, added installer for glazy theme

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 touch .installed
 
 # Dependencies
-sudo apt install -y curl coreutils micro python3 pipx wget zoxide zsh
+sudo apt install -y curl coreutils micro python3 wget zoxide zsh
 
 # Oh My Zsh
 cp ./dotfiles/.zshrc ~/.zshrc
@@ -25,15 +25,25 @@ wget -O- "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install
 # https://www.mate-look.org/p/1084939
 sudo mkdir -p /usr/share/icons/OSX-ElCapitan
 
-# https://unix.stackexchange.com/a/743991
-# this took me forever
-# get JSON file that contains download info
+## https://unix.stackexchange.com/a/743991
+## this took me forever
+### get JSON file that contains download info
 curl -Lfs "https://www.mate-look.org/p/1084939/loadFiles" | \
-	# parse that JSON, get the most recent version's download url (in this case, the last returned item)
+	### parse that JSON, get the most recent version's download url (in this case, the last returned item)
 	jq -r '.files | .[-1] | .url' | \
-	# parse junk characters
+	### parse junk characters
 	perl -pe 's/\%(\w\w)/chr hex $1/ge' | \
-	# download the file
+	### download the file
 	xargs wget -O- | \
-	# extract the cursors to the global icons folder
+	### extract the cursors to the global icons folder
 	sudo tar -xjf - -C /usr/share/icons/OSX-ElCapitan --strip-components=2
+
+# Glazy Metacity Theme by NOVOMENTE
+# https://www.mate-look.org/s/Mate/p/1007198
+mkdir -p ~/.themes
+
+# download theme pack
+curl -Lfs \
+	"https://sourceforge.net/projects/novomente-themes/files/Metacity/Glazy_pack.tar.gz/download" | \
+	# extract pack, extract each individual theme
+	tar --to-command='tar -xzf - -C ~/.themes' -xzf - --exclude="README*" --strip-components=1
