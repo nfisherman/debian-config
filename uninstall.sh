4fbb7f9a1b60e90d0e47b67b2fe5fd8f933e7f2a
nfisherman <ca.lehman05@gmail.com>
2024-05-27 15:36:45 +0000

    created an uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Root check
+if [ "$(id -u)" -eq 0 ]; then
+	echo "Cannot be run as root. Run as a normal user with sudo priveleges."
+	exit
+fi
+
+response='RAHHHHH'
+while [ "${1}" != '-y' ] && [ "${response}" != 'y' ] && \
+  [ "${response}" != 'n' ] && [ "${response}" != '' ]; do
+	printf "Are you sure you want to uninstall? [y/N] " >&2
+	read -r response
+	response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+done
+
+if [ "${response}" = 'n' ] || [ "${response}" = '' ]; then
+	echo "Aborting."
+    exit
+fi
+
+echo "Uninstalling..."
+
+sudo rm -rf /usr/share/icons/OSX-ElCapitan
+sudo rm -rf /usr/share/icons/Oxylite
+sudo rm -rf /usr/share/icons/Mint-X*
+sudo rm -rf /usr/share/icons/dist-icons
+sudo rm -rf /usr/share/Skewaita
+
+rm -rf ~/.icons/MASTER
+rm -rf ~/.themes/Glazy*
+
+echo "Themes have been uninstalled."
