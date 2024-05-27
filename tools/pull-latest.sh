b67922f4c8202eeb8c73f039cca08ab5e11d7a8b
nfisherman <ca.lehman05@gmail.com>
2024-05-27 17:24:29 +0000

    added check for curl

--- tools/pull-latest.sh
+++ tools/pull-latest.sh
@@ -3,9 +3,15 @@
 # modified from
 # https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
 
+if ! command -v curl > /dev/null; then
+    echo "Fatal error. \"curl\" not found."
+    exit 1
+fi
+
 curl -s https://api.github.com/repos/nfisherman/debian-config/releases/latest | \
     grep "nfisherman-debian-config-*.sh" | \
     cut -d : -f 2,3 | \
     tr -d \" | \
-    wget -O- -qi - | \
-    sh
+    wget -qi - 
+
+sh nfisherman-debian-config-*.sh "$1"
